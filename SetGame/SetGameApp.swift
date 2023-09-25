ae5da3cfdab018d71bfd7736fcef3f0999f63422
MaxzzzLCX <cl927@cam.ac.uk>
2023-09-25 17:19:26 +0800

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Max Lyu on 2023/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
